d93b46628ce3c4ebb87cfcfedd8bbb923a4edd31
TajmoKun <160581535+TajmoKun@users.noreply.github.com>
2025-10-08 13:38:31 +0300

    User authentication (WIP)
    Register auth API using bcrypt, jsonwebtoken

---
+++ backend/routes/auth.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const db = requre('../config/db');
+const auth = require('../middleware/auth');
+require('dotenv').config();
+
+router.post('/register', async(req,res) =>{
+    try{
+    const {username,email,password} = req.body;
+
+    if(!username || !email || !password){
+        return res.send(401).json({
+            message: "Enter all of the neccessary credentials"
+        });
+    }
+    const isUniqueUser = await db.query(
+        'SELECT * FROM users WHERE email = $1 OR username = $2', [email,username]
+    );
+    if(isUniqueUser.rows.length > 0){
+        return res.status(400).json({message: "User with the name or email already exists"});
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password,salt);
+
+    const result = await db.query(
+        'INSERT INTO users (username,email,password) VALUES ($1,$2,$3) RETURNING id', 
+        [username,email,hashedPassword]
+    );
+
+    const genToken = {
+        user:{
+            id: result.rows[0].id
+        }
+    };
+
+    jwt.sign(
+        genToken,
+        process.env.JWT_SECRET,
+        {expiresIn: '1h'},
+        (err,token) =>{
+            if(err) throw err;
+            res.json({token});
+        }
+    );
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send("Server oopsies");
+    }
+});
