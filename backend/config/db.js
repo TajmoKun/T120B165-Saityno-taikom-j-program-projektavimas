b473292ed917fc91d93b1e07d50ae7468f891dff
TajmoKun <160581535+TajmoKun@users.noreply.github.com>
2025-10-08 10:29:22 +0300

    Quick fix

--- backend/config/db.js
+++ backend/config/db.js
@@ -9,21 +9,6 @@ const pool = new Pool({
   database: String(process.env.DB_NAME)
 });
 
-
-// pool.query(`
-//   CREATE TABLE IF NOT EXISTS users (
-//     id SERIAL PRIMARY KEY,
-//     username VARCHAR(50) UNIQUE NOT NULL,
-//     email VARCHAR(255) UNIQUE NOT NULL,
-//     password VARCHAR(255) NOT NULL,
-//     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-//   )
-// `).then(() => {
-//   console.log('Users table created or already exists');
-// }).catch(err => {
-//   console.error('Error creating users table:', err);
-// });
-
 module.exports = {
   query: (text, params) => pool.query(text, params),
 };
